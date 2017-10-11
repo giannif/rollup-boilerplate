8a16f17055e840204840a4521bacbf271ff8e571
giannif <giannif@gmail.com>
2017-10-11 09:09:32 -0700

    build(update): deps, rollup config, travis

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# Rollup boilerplate with babel and css modules 
-
 ### dev 
  - `npm run start` to run [serve](https://github.com/zeit/serve)
  - `npm run dev` to run rollup with the `-w` watch flag
  - Change code, refresh the browser 
 
- ### TODO 
- - `async` not working without warnings, taking too much config
+ ### deploy
+ Deployment is automatically done by Travis using tags. To deploy a new script, simply create a tag, push it, and when the build is done you will be able to see it at `https://s3Domain/s3Bucket/tag-name/bundle.js`.
+
+You can use a `latest` tag for distribution if you'd like. To re-deploy to this tag, delete the tag, recreate, and push to master. The latest bundle will then be updated and the cache cleared.
+
+ ### re-deploying a tag
+ - `git -d tag TAG_NAME`
+ - `git push origin :refs/tags/TAG_NAME`
+ - `git tag TAG_NAME`
+ - `git push --tags`
--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,56 @@
 import resolve from 'rollup-plugin-node-resolve'
+import builtIns from 'rollup-plugin-node-builtins'
+import globals from 'rollup-plugin-node-globals'
 import commonjs from 'rollup-plugin-commonjs'
 import uglify from 'rollup-plugin-uglify'
 import babel from 'rollup-plugin-babel'
 import postcss from 'rollup-plugin-postcss'
+import json from 'rollup-plugin-json'
 import postcssModules from 'postcss-modules'
+import postcssNested from 'postcss-nested'
 import autoprefixer from 'autoprefixer'
+import FastlyPurge from 'fastly-purge'
+import sizes from 'rollup-plugin-sizes'
 
 // `npm run --production build` -> `production` is true
 // `npm run dev` -> `production` is false
 
 const production = process.env.NODE_ENV === 'production'
+const { TRAVIS_BRANCH, TRAVIS_BUILD_NUMBER, TRAVIS_TAG } = process.env
+
+let deployDir = 'public'
+if (TRAVIS_BUILD_NUMBER) {
+    const s3Domain = `your.scripts.com`
+    const s3Bucket = 'your-bucket'
+    // set up the build dir which will be pushed to s3
+    // .travis.yml configures s3 to push anything in public/
+    const s3Path = TRAVIS_TAG ? `/${TRAVIS_TAG}` : `/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}`
+    // if we build a tag, we want to purge
+    if (TRAVIS_TAG) {
+        const fastlyPurge = new FastlyPurge()
+        const filesToPurge = [`bundle.js`, `bundle.js.map`]
+        filesToPurge.forEach(file => {
+            fastlyPurge.url(`http://${s3Domain}/${s3Bucket}${s3Path}/${file}`, () => {})
+            fastlyPurge.url(`https://${s3Domain}/${s3Bucket}${s3Path}/${file}`, () => {})
+        })
+    }
+    deployDir += s3Path
+}
 
 const cssExportMap = {}
 export default {
-    entry: 'src/main.js',
-    dest: 'public/bundle.js',
-    format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+    input: 'src/main.js',
+    output: {
+        file: `${deployDir}/bundle.js`,
+        format: 'iife',
+    }, // immediately-invoked function expression — suitable for <script> tags
     plugins: [
-        resolve(), // tells Rollup how to find node_modules
+        // tells Rollup how to find node_modules
+        resolve({
+            // whether to prefer built-in modules (e.g. `fs`, `path`) or
+            // local ones with the same names
+            preferBuiltins: true, // Default: true (specify explicitly to disable warning)
+        }),
         postcss({
             plugins: [
                 autoprefixer(),
@@ -26,6 +59,7 @@ export default {
                         cssExportMap[id] = exportTokens
                     },
                 }),
+                postcssNested(),
             ],
             getExportNamed: true, // Default false, when set to true it will also named export alongside default export your class names
             getExport(id) {
@@ -38,11 +72,16 @@ export default {
         }),
         babel({
             exclude: 'node_modules/**', // only transpile our source code
+            runtimeHelpers: true,
         }),
+        json(),
         commonjs(), // converts node_modules to ES modules
+        globals(),
+        builtIns(),
+        production && sizes(),
         production && uglify(), // minify, but only in production
     ],
-    sourceMap: true,
+    sourcemap: true,
     watch: {
         include: 'src/**',
     },
